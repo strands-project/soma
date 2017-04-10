c579525c8fc8fdac6b5df1817652f23a604f1dd5
Ferdian Jovan <fxj345@cs.bham.ac.uk>
2017-04-10 21:52:32 +0100

    add soma trajectory msg and basic functionality for viewing trajectories based on time

---
+++ soma_trajectory/scripts/trajectory_visualisation.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import rospy
+from soma_trajectory.srv import SOMAQueryTrajectories, SOMAQueryTrajectoriesRequest
+
+
+def average_velocity(traj):
+    start = traj.start_time
+    end = traj.end_time
+    delta = float((end-start).secs + 0.000000001 * (end-start).nsecs)
+    avg_vel = 0.0
+    if delta != 0.0:
+        avg_vel = traj.length / delta
+    return avg_vel
+
+
+if __name__ == "__main__":
+    rospy.init_node("soma_trajectory_visualisation")
+    rospy.loginfo("Wait for soma/query_trajectories service...")
+    srv = rospy.ServiceProxy("soma/query_trajectories", SOMAQueryTrajectories)
+    srv.wait_for_service()
+    rospy.loginfo("Requesting visualisation for all trajectories...")
+    res = srv(SOMAQueryTrajectoriesRequest(visualize=True))
+    trajectories = res.trajectories
+
+    average_length = 0.0
+    longest_length = -1.0
+    short_trajectories = 0
+    average_vel = 0.0
+    highest_vel = -1.0
+
+    for traj in trajectories:
+        average_length += float(traj.length)
+        avg_vel = average_velocity(traj)
+        average_vel += average_velocity(traj)
+        if traj.length < 1:
+            short_trajectories += 1
+        if longest_length < traj.length:
+            longest_length = traj.length
+        if highest_vel < avg_vel:
+            highest_vel = avg_vel
+
+    average_length /= float(len(trajectories))
+    average_vel /= float(len(trajectories))
+    rospy.loginfo("Average length of tracks is " + str(average_length))
+    rospy.loginfo("Longest length of tracks is " + str(longest_length))
+    rospy.loginfo("Short trajectories are " + str(short_trajectories))
+    rospy.loginfo("Average velocity of tracks is " + str(average_vel))
+    rospy.loginfo("Highest velocity of tracks is " + str(highest_vel))
+    rospy.loginfo("Total number of trajectories: %d" % len(trajectories))
+    raw_input("Press 'Enter' to exit.")
---
+++ soma_trajectory/src/soma_trajectory/visualisation.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import ColorRGBA
+from geometry_msgs.msg import Point
+import interactive_markers.interactive_marker_server as ims
+from visualization_msgs.msg import Marker, InteractiveMarkerControl
+from interactive_markers.interactive_marker_server import InteractiveMarker
+
+
+class TrajectoryVisualisation(object):
+
+    def __init__(self, marker_name):
+        self._modulo = 3
+        self._visualised_ids = list()
+        self._server = ims.InteractiveMarkerServer(marker_name)
+        rospy.Timer(rospy.Duration(5), self._remove_trajectories)
+
+    def _remove_trajectories(self, event):
+        now = rospy.Time.now()
+        remove_ids = list()
+        for idx in range(len(self._visualised_ids)):
+            if now - self._visualised_ids[idx][1] > rospy.Duration(60):
+                self._server.erase(self._visualised_ids[idx][0])
+                self._server.applyChanges()
+                remove_ids.append(self._visualised_ids[idx][0])
+        # rospy.loginfo("Removing %d trajectories from visualisation..." % len(remove_ids))
+        self._visualised_ids = [
+            i for i in self._visualised_ids if i[0] not in remove_ids
+        ]
+
+    def visualize_trajectories(self, trajectories):
+        for trajectory in trajectories:
+            self._visualised_ids.append((trajectory.id, rospy.Time.now()))
+            int_marker = self.create_trajectory_marker(trajectory)
+            self._server.insert(int_marker, self._update_cb)
+            self._server.applyChanges()
+
+    def _update_cb(self, feedback):
+        return
+
+    def set_line_color(self, **args):
+        color = ColorRGBA()
+        color.r = 0.0
+        color.g = float(args["index"]) / float(args["length"])
+        color.b = 1.0 - float(args["index"]) / float(args["length"])
+        color.a = 1.0
+        return color
+
+    def create_trajectory_marker(self, traj):
+        # create an interactive marker for our server
+        int_marker = InteractiveMarker()
+        int_marker.header.frame_id = "/map"
+        int_marker.name = traj.id
+        int_marker.description = traj.id
+
+        int_marker.pose = traj.posearray.poses[0]
+        int_marker.pose.orientation.x = 0
+        int_marker.pose.orientation.y = 0
+        int_marker.pose.orientation.z = 0
+        int_marker.pose.orientation.w = 1
+
+        line_marker = Marker()
+        line_marker.type = Marker.LINE_STRIP
+        line_marker.scale.x = 0.05
+
+        line_marker.points = []
+        while len(traj.posearray.poses) / self._modulo > 5000:
+            self._modulo += 1
+
+        for i, pose in enumerate(traj.posearray.poses):
+            if i % self._modulo == 0:
+                p = Point(
+                    pose.position.x - int_marker.pose.position.x,
+                    pose.position.y - int_marker.pose.position.y,
+                    0.0
+                )
+                line_marker.points.append(p)
+                line_marker.colors.append(
+                    self.set_line_color(index=i, length=len(traj.posearray.poses))
+                )
+
+        self._modulo = 3
+
+        # create a control which will move the box
+        # this control does not contain any markers,
+        # which will cause RViz to insert two arrows
+        control = InteractiveMarkerControl()
+        control.markers.append(line_marker)
+        int_marker.controls.append(control)
+
+        return int_marker
